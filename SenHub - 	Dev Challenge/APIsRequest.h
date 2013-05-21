91d9aee092d449fb7f040fc2db9872490717594f
Florian Reiss <85802@supinfo.com>
2013-05-20 21:40:19 -0700

    add APIsRequest

---
+++ SenHub - 	Dev Challenge/APIsRequest.h
@@ -0,0 +1,13 @@
+//
+//  APIsRequest.h
+//  SenHub - 	Dev Challenge
+//
+//  Created by Florian Reiss on 20/05/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface APIsRequest : NSObject
+
+@end
---
+++ SenHub - 	Dev Challenge/APIsRequest.m
@@ -0,0 +1,13 @@
+//
+//  APIsRequest.m
+//  SenHub - 	Dev Challenge
+//
+//  Created by Florian Reiss on 20/05/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#import "APIsRequest.h"
+
+@implementation APIsRequest
+
+@end
